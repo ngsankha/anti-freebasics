9bb92d40739ac90a405887e4e9f088545137442b
Sankha Narayan Guria <sankha93@gmail.com>
2015-09-28 23:52:47 +0530

    Initial working version

---
+++ script.js
@@ -0,0 +1,67 @@
+function click(elem) {
+  var evt = document.createEvent('Event');
+  evt.initEvent('click', true, true);
+  elem.dispatchEvent(evt);
+}
+
+function openFileDialog() {
+  var fileElem = document.getElementById('fileElem');
+  fileElem.click();
+}
+
+function loadPicture(evt) {
+  var file = evt.target.files[0];
+  if (!file.type.match('image.*')) {
+    // TODO: Fix this later
+    alert('Only images are allowed');
+  } else {
+    var reader = new FileReader();
+    reader.addEventListener('load', loadOnCanvas(file), false);
+    reader.readAsDataURL(file);
+  }
+}
+
+var logoPreloader = new Image();
+logoPreloader.src = 'logo.png';
+
+function imageLoadTracker(src, onload) {
+  var img = new Image();
+  img.addEventListener('load', onload, false);
+  img.src = src;
+  return img;
+}
+
+function loadOnCanvas(file) {
+  return function(e) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.globalAlpha = 1.0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var imagesLoaded = 0;
+    var imageProcess = function() {
+      imagesLoaded += 1;
+      if (imagesLoaded == 2) {
+        var height = baseImg.height, width = baseImg.width;
+        var newWidth, newHeight, scale;
+        if (width < height) {
+          scale = width / 800;
+          newWidth = 800;
+          newHeight = height / scale;
+        } else {
+          scale = height / 800;
+          newHeight = 800;
+          newWidth = width / scale;
+        }
+        ctx.drawImage(baseImg, canvas.width / 2 - newWidth / 2,
+          canvas.height / 2 - newHeight / 2, newWidth, newHeight);
+        ctx.globalAlpha = 0.7;
+        ctx.drawImage(overlayImage, 0, 0);
+      }
+    }
+    var baseImg = imageLoadTracker(e.target.result, imageProcess);
+    var overlayImage = imageLoadTracker('logo.png', imageProcess);
+  }
+}
+
+document.getElementById('select-photo').addEventListener('click', openFileDialog, false);
+document.getElementById('fileElem').addEventListener('change', loadPicture, false);
