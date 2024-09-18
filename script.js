16d5b7d8adc3552cef321e93116062538b917659
Sankha Narayan Guria <sankha93@gmail.com>
2024-09-18 00:57:22 -0500

    linkedin overlay

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function loadPicture(evt) {
 }
 
 var logoPreloader = new Image();
-logoPreloader.src = 'logo.png';
+logoPreloader.src = 'li-overlay.png';
 
 function imageLoadTracker(src, onload) {
   var img = new Image();
@@ -30,10 +30,27 @@ function imageLoadTracker(src, onload) {
   return img;
 }
 
+function fillTextCircle(ctx, text, x, y, radius, startRotation) {
+   var numRadsPerLetter = 0.5 * Math.PI / text.length;
+   ctx.save();
+   ctx.translate(x,y);
+   ctx.rotate(startRotation);
+
+   for(var i=0;i<text.length;i++){
+      ctx.save();
+      ctx.rotate(i*numRadsPerLetter);
+
+      ctx.fillText(text[i],0,-radius);
+      ctx.restore();
+   }
+   ctx.restore();
+}
+
 function loadOnCanvas(file) {
   return function(e) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
+    ctx.reset()
     ctx.globalAlpha = 1.0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var imagesLoaded = 0;
@@ -43,33 +60,45 @@ function loadOnCanvas(file) {
         var height = baseImg.height, width = baseImg.width;
         var newWidth, newHeight, scale;
         if (width < height) {
-          scale = width / 800;
-          newWidth = 800;
+          scale = width / 1024;
+          newWidth = 1024;
           newHeight = height / scale;
         } else {
-          scale = height / 800;
-          newHeight = 800;
+          scale = height / 1024;
+          newHeight = 1024;
           newWidth = width / scale;
         }
         ctx.drawImage(baseImg, canvas.width / 2 - newWidth / 2,
           canvas.height / 2 - newHeight / 2, newWidth, newHeight);
-        var imageData = ctx.getImageData(0, 0, 800, 800);
+        var imageData = ctx.getImageData(0, 0, 1024, 1024);
         var data = imageData.data;
 
-        for(var i = 0; i < data.length; i += 4) {
-          var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-          data[i] = brightness;
-          data[i + 1] = brightness;
-          data[i + 2] = brightness;
-        }
-        ctx.putImageData(imageData, 0, 0);
-        ctx.globalAlpha = 0.7;
+        // for(var i = 0; i < data.length; i += 4) {
+        //   var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+        //   data[i] = brightness;
+        //   data[i + 1] = brightness;
+        //   data[i + 2] = brightness;
+        // }
+        // ctx.putImageData(imageData, 0, 0);
+        // ctx.globalAlpha = 0.7;
+
+        ctx.globalCompositeOperation='destination-in';
+        ctx.beginPath();
+        ctx.arc(newWidth / 2, newHeight / 2, newHeight / 2, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.globalCompositeOperation='source-over';
         ctx.drawImage(overlayImage, 0, 0);
+
+        // ctx.font = "bold 30px";
+        // fillTextCircle(ctx, "Circle Text ", 0, 800 / 2, 800 / 2, Math.PI / 2);
+
         document.getElementById('download-btn').style.display = 'inline';
       }
     }
     var baseImg = imageLoadTracker(e.target.result, imageProcess);
-    var overlayImage = imageLoadTracker('logo.png', imageProcess);
+    var overlayImage = imageLoadTracker('li-overlay.png', imageProcess);
   }
 }
 
